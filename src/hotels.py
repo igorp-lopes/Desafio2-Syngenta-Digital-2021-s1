023c09c15e4bf7edec34b7076a0bf540feb46937
igorp-lopes <igorp.lopes16@gmail.com>
2021-04-24 20:56:52 -0300

    Criada a classe Hotel

---
+++ src/hotels.py
@@ -0,0 +1,7 @@
+class Hotel:
+
+    def __init__(self, name, classification, tax_week, tax_weekend):
+        self.name = name
+        self.classification = classification
+        self.tax_week = tax_week
+        self.tax_weekend = tax_weekeend
