c4a184ba077519f13be5e019a0d31d597180e709
igorp-lopes <igorp.lopes16@gmail.com>
2021-04-25 00:08:28 -0300

    Criado método de tratamento do input

--- src/hotels.py
+++ src/hotels.py
@@ -1,3 +1,4 @@
+import re
 class Hotel:
 
     def __init__(self, name, classification, tax_week, tax_weekend):
@@ -11,4 +12,18 @@ class HotelSorter:
 
     def __init__(self, input, hotel_list):
         self.hotel_list = hotel_list
+        self.input = input
         pass
+
+    def input_parser(self, input):
+        """
+        Method that parses the essential data from the input
+        """
+
+        # Extracting the client type
+        client_type = input.split(':')[0]
+
+        # Extracting the dates between brackets using regex
+        dates = re.findall(r'\(.*?\)', input)
+
+        # We transform our dates
