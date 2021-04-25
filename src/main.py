58eeb24793e31555545bb55976f3fab87cd9035d
igorp-lopes <igorp.lopes16@gmail.com>
2021-04-25 20:27:36 -0300

    Finalizado o programa principal

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ hotel_list = [Hotel("Lakewood", 3, 110, 90, 80, 80), Hotel(
     "Bridgewood", 4, 160, 60, 110, 50), Hotel("Ridgewood", 5, 220, 150, 100, 40)]
 
 # We receive the input
-input_ = input('Entre com a informacao da sua reserva:\n')
+input_ = input('Entre com as informacoes da sua reserva:\n')
 
 # Based on the input, we determine the ideal hotel to choose
 ht_sorter = HotelSorter(input_, hotel_list)
