1bcf04b0551b14ee0825921990b2d8dcdcfb7bd4
igorp-lopes <igorp.lopes16@gmail.com>
2021-04-25 00:08:54 -0300

    Criado programa de teste para a classe HotelSorter

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from hotels import Hotel, HotelSorter
+
+sorting = HotelSorter(
+    "Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)", [])
+
+sorting.input_parser(
+    "Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)")
